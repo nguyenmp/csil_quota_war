dd474b36f89a1dd077fc8f38cdc719b318b5b128
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-04-28 03:05:54 -0700

    Garbage collect class files

--- clean.sh
+++ clean.sh
@@ -7,5 +7,13 @@ gits="$(find | grep '\.git$')"
 home="$(pwd)"
 while read -r git; do
     echo "Cleaning $git"
-    res="$(cd $home && cd ${git::-4} && git gc)"
+    res="$(cd ${git::-4} && git gc && cd $home)"
 done <<< "$gits"
+
+# Garbage collects all your compiled java code
+# This can get really intense when you take 162
+classes="$(find | grep '\.class$')"
+while read -r class; do
+    echo "Cleaning $class"
+    res="$(rm $class)"
+done <<< "$classes"
